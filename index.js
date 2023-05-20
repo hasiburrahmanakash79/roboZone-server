e1b8f784fbc26a9e83f7c272a34d9f8558c6812b
hasiburrahmanakash79 <hasiburrahmanakash79@gmail.com>
2023-05-20 13:23:36 +0600

    get data using email

--- index.js
+++ index.js
@@ -48,6 +48,16 @@ async function run() {
       res.send(result)
     })
 
+    app.get('/myToy', async(req, res) => {
+      let query = {}
+      if(req.query?.email){
+        query = {email: req.query.email}
+      }
+      const cursor =  toyCollection.find(query)
+      const result = await cursor.toArray()
+      res.send(result)
+    })
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
