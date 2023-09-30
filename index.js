36242d81cd0417bd652a802cac132b7cac1f9647
hasiburrahmanakash79 <hasiburrahmanakash79@gmail.com>
2023-09-30 11:59:10 +0600

    makeuser delete api

--- index.js
+++ index.js
@@ -105,6 +105,14 @@ async function run() {
       res.send(result);
     });
 
+    // delete user
+    app.delete("/user/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await usersCollection.deleteOne(query);
+      res.send(result);
+    });
+
     app.get("/users/admin/:email", async (req, res) => {
       const email = req.params.email;
 
